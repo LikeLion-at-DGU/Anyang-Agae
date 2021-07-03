d29ff69876b00eac862572ec71054fc48ba64bb1
Seulgi_LEE <dl4459@naver.com>
2021-07-03 20:22:56 +0900

    [refactor] main, signup css update

--- anyang_agae/views.py
+++ anyang_agae/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from reviews.models import Review
 
 def main(request):
-  return render(request, 'main.html')
+  review = Review.objects.all().order_by('-pub_date')
+  reviews = review[:3]
+  return render(request, 'main.html', {'reviews': reviews})
