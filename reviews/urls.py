31c20c39ec5d1f7f85ff272682c85bd626a98c60
Seulgi_LEE <dl4459@naver.com>
2021-08-09 22:01:10 +0900

    [feat] map api search func

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from .views import *
 
-app_name="reviews"
+app_name = "reviews"
 urlpatterns = [
-    path('write/', write, name="write"), #게시글 작성페이지
-    path('ReviewList/',ReviewList, name="ReviewList"), #게시글 목룍 페이지   
-    path('SearchReview/',SearchReview, name="SearchReview"), #게시글 목룍 페이지   
-    path('<str:id>',ReviewDetail, name="ReviewDetail"), #게시글 자세히보는 페이지
-    path('create/',create, name="create"), #게시글 작성함수
+    path('write/', write, name="write"),  # 게시글 작성페이지
+    path('ReviewList/', ReviewList, name="ReviewList"),  # 게시글 목룍 페이지
+    path('SearchReview/', SearchReview, name="SearchReview"),  # 게시글 목룍 페이지
+    path('<str:id>', ReviewDetail, name="ReviewDetail"),  # 게시글 자세히보는 페이지
+    path('create/', create, name="create"),  # 게시글 작성함수
     # path('delete/',delete, name="delete"), #게시글 삭제함수
-    path('<str:id>/create_comment', create_comment, name="create_comment"), #댓글 작성함수
+    path('<str:id>/create_comment', create_comment,
+         name="create_comment"),  # 댓글 작성함수
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -3,12 +3,15 @@ from .models import Review, comment
 from django.db.models import Q
 from django.utils import timezone
 
+
 def write(request):
     return render(request, 'reviews/write.html')
 
+
 def SearchReview(request):
     reviews = Review.objects.all()
-    return render(request, 'reviews/SearchReview.html',{'reviews':reviews})
+    return render(request, 'reviews/SearchReview.html', {'reviews': reviews})
+
 
 def ReviewList(request):
     reviews = Review.objects.all()
@@ -16,29 +19,33 @@ def ReviewList(request):
     # 검색
     if kw:
         reviews = reviews.filter(
-            Q(title__icontains=kw) | # 제목검색
-            Q(writer__username__icontains=kw) # writer색
+            Q(title__icontains=kw) |  # 제목검색
+            Q(writer__username__icontains=kw)  # writer색
         ).distinct()
-    return render(request, 'reviews/ReviewList.html',{'reviews':reviews, 'kw' : kw})
+    return render(request, 'reviews/ReviewList.html', {'reviews': reviews, 'kw': kw})
+
 
 def create(request):
     new_reivew = Review()
     new_reivew.title = request.POST['title']
     new_reivew.pub_date = timezone.now()
     new_reivew.content = request.POST['content']
-    new_reivew.writer=request.user
+    new_reivew.writer = request.user
     new_reivew.save()
     return redirect('reviews:ReviewDetail', new_reivew.id)
 
+
 def ReviewDetail(request, id):
-    review = get_object_or_404(Review, pk =id)
+    review = get_object_or_404(Review, pk=id)
     all_comments = review.comments.all().order_by('-created_at')
-    return render(request, 'reviews/ReviewDetail.html', {'review': review, 'comments':all_comments})
+    return render(request, 'reviews/ReviewDetail.html', {'review': review, 'comments': all_comments})
+
 
 def create_comment(request, id):
     if request.method == "POST":
         review = get_object_or_404(Review, pk=id)
         current_user = request.user
         content = request.POST.get('content')
-        comment.objects.create(content=content, writer=current_user, review=review)
+        comment.objects.create(
+            content=content, writer=current_user, review=review)
     return redirect('reviews:ReviewDetail', id)
