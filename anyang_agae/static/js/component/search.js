e2d1c654b6428751e7663fffe15e0399bf235743
SJLEE316 <lsj8249@naver.com>
2021-08-10 22:03:12 +0900

    fix:Search + pagination 동시에 가능

--- anyang_agae/static/js/component/search.js
+++ anyang_agae/static/js/component/search.js
@@ -16,3 +16,22 @@ function search() {
     search_form.submit();
   }
 }
+
+$(document).ready(function () {
+  $(".page-link").on("click", function () {
+    $("#page").val($(this).data("page"));
+    $("#searchForm").submit();
+  });
+
+  // $("#btn_search").on('click', function() {
+  //     $("#kw").val($(".kw").val());
+  //     $("#page").val(1);
+  //     $("#searchForm").submit();
+  // });
+
+  // $(".so").on("change", function () {
+  //   $("#so").val($(this).val());
+  //   $("#page").val(1);
+  //   $("#searchForm").submit();
+  // });
+});
--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Review, Comment
-from django.db.models import Q
+from django.db.models import Q, Count
 from django.utils import timezone
 from django.core.paginator import Paginator
 
@@ -12,22 +12,33 @@ def SearchReview(request):
     return render(request, 'reviews/SearchReview.html',{'reviews':reviews})
 
 def ReviewList(request):
-    reviews = Review.objects.all()
+    reviews = Review.objects.all().order_by('-pub_date')
+    # 검색
+    kw = request.GET.get('kw', '')  # 검색어
+    if kw:
+        reviews = reviews.filter( # reviews로 받고 아래에서 page당 3개씩 받는 원리
+            Q(title__icontains=kw) | # 제목검색
+            Q(writer__username__icontains=kw) # writer검색
+        ).distinct()
+
     # pagination
     paginator = Paginator(reviews, 3)
     page = request.GET.get('page', '1')  # 페이지
     page_obj = paginator.get_page(page)
 
-    # 검색
-    kw = request.GET.get('kw', '')  # 검색어
-    if kw:
-        reviews = reviews.filter(
-            Q(title__icontains=kw) | # 제목검색
-            Q(writer__username__icontains=kw) # writer색
-        ).distinct()
 
-    context = {'reviews':reviews, 'kw' : kw, 'page_obj' : page_obj}
-    # 'page_obj' : page_obj, 'page' : page
+    # 정렬
+    # sort = request.GET.get('sort','recent')
+    # if sort == 'recommend': # 추천 순
+    #     reviews = Review.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-pub_date')
+    # elif sort == 'click': # 클릭 순
+    #     reviews = Review.objects.annotate(num_view=Count('view_count')).order_by('-num_view', '-pub_date')
+    # elif sort == 'review': # 리뷰 순
+    #     reviews = Review.objects.annotate(num_review=Count('comments')).order_by('-num_review', '-pub_date')
+    # else: # 최신 순
+    #     reviews = Review.objects.order_by('-pub_date')
+
+    context = {'reviews':page_obj, 'kw' : kw, 'page':page}
     return render(request, 'reviews/ReviewList.html', context)
 
 def create(request):
